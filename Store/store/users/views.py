1a158c75938869f259df293a034d85ea6c9f80c7
SanyaWenzy <letssanya@gmail.com>
2026-03-26 22:46:58 +0200

    Update6
    Everything for the following updates until I write stop updates 6, this will all be for update 6, you just can't drop everything at once :(

--- Store/store/users/views.py
+++ Store/store/users/views.py
@@ -1,8 +1,33 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib import auth
+from users.models import User
+from users.forms import UserLoginForm, UserRegistrationForm
+from django.urls import reverse
 
 # Create your views here.
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
 
 def registration(request):
-    return render(request, 'users/registration.html')
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm()
+    context = {'form': form}
+    return render(request, 'users/registration.html', context)
